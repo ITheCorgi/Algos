4ff48debdabbbb12731586f163f4612c3dc63c8c
Max <xmavrik74@gmail.com>
2021-04-23 15:33:24 +0500

    Update binarysearch.go

--- Binary-Search/binarysearch.go
+++ Binary-Search/binarysearch.go
@@ -1,16 +1,16 @@
+//BinarySearch algorithm implementation
+//Time complexity is O(logn)
 package binarysearch
 
 import (
 	"fmt"
 )
 
-//BinarySearch algorithm implementation
-//Time complexity is O()
 func BinarySearch(container []int, target int) int {
 	start, end := 0, len(container) - 1
 
 	for start < end {
-		median := int(start + (end - start))/2
+		median := start + (end - start)/2
 		if container[median] > target {
 			end = median - 1
 		} else if container[median] < target {
