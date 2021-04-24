2cc5a711e5fe84fbf1f3b2966f2258c77b6fdf27
Max Gustov <maxim.gustov@emerson.com>
2021-04-24 22:44:43 +0500

    Draft

--- Insertion-Sort/insertionsort.go
+++ Insertion-Sort/insertionsort.go
@@ -5,3 +5,14 @@ import (
 	"fmt"
 )
 
+func insertionSort(in []int) {
+	for i := 1; i < len(in); i++ {
+		j := i
+		for j > 0 {
+			if in[j-1] > in[j] {
+				in[j-1], in[j] = in[j], in[j-1]
+			}
+			j--
+		}
+	}
+}
---
+++ Insertion-Sort/insertionsort_test.go
@@ -0,0 +1,47 @@
+package insertionsort
+
+import (
+	"fmt"
+	"testing"
+	"reflect"
+)
+
+func insertionSortTest(t *testing.T) {
+	testcases := []struct {
+		Name     string
+		Input    []int
+		Expected []int
+	}{
+		{
+			"Empty slice",
+			[]int{},
+			[]int{},
+		},
+		{
+			"One element dimension",
+			[]int{1},
+			[]int{1},
+		},
+		{
+			"Slice with numbers",
+			[]int{1, 10, 2, 9, 3, 8, 4, 7, 5, 6},
+			[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+		},
+		{
+			"Slice with repeatable numbers",
+			[]int{1, 10, 2, 9, 3, 8, 4, 7, 5, 6, 1, 10, 2, 9, 3, 8, 4, 7, 5, 6, 1, 10, 2, 9, 3, 8, 4, 7, 5, 6, 1, 10, 2, 9, 3, 8, 4, 7, 5, 6, 1, 10, 2, 9, 3, 8, 4, 7, 5, 6},
+			[]int{1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 9, 9, 9, 9, 9, 10, 10, 10, 10, 10},
+		},
+	}
+
+	for _, testcase := range testcases {
+		t.Run(testcase.Name, func(t *testing.T) {
+			var got []int
+			insertionSort(testcase.Input)
+			got = testcase.Input
+			if !reflect.DeepEqual(testcase.Expected, got) {
+				t.Errorf("Test \"%s\" failed.\nExpected: %v\n Got: %v", testcase.Name, testcase.Expected, got)
+			}
+		})
+	}
+}
